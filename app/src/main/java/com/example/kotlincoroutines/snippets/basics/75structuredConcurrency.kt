c0c08d43271503be2b425e90df46f4615c212d01
sagarpatel288 <sagar.patel@sharecare.com>
2025-02-13 16:17:57 +0530

    Document Structured Concurrency in Kotlin Coroutines.
    This commit introduces a new example illustrating structured concurrency within Kotlin coroutines.
    It explains the concept of structured concurrency as the ability to create a hierarchy of coroutines, including parent-child and sibling relationships.
    The example demonstrates this hierarchy using `GlobalScope.launch` and nested `launch` coroutines, showcasing how multiple coroutines can be launched within each other, forming a structured hierarchy.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/basics/75structuredConcurrency.kt
@@ -0,0 +1,101 @@
+package com.example.kotlincoroutines.snippets.basics
+
+import kotlinx.coroutines.*
+
+/**
+ * Earlier, we have seen what is concurrency.
+ * [Concurrency](app/src/main/java/com/example/kotlincoroutines/snippets/basics/030explicitWaitForGlobalScope.kt)
+ *
+ * Again, concurrency is an ability to run and deal with multiple tasks,
+ * either using a synchronous programming (multi-threaded) or asynchronous programming (non-blocking multi-process).
+ *
+ * In this small practical, we will see and experience structured concurrency.
+ *
+ * Now, what is a structured concurrency?
+ *
+ * A structured concurrency means an ability to provide a hierarchy of coroutines.
+ * For example, one coroutine inside another coroutine, one coroutine after another coroutine, and so on.
+ *
+ * So, we can provide a structure, a hierarchy to the coroutines.
+ * Hence, we call it a structured concurrency.
+ *
+ * In a formal way, sometimes we call it parent-children, and sibling coroutines.
+ */
+fun main() = runBlocking {
+
+    // `runBlocking` is a coroutine scope + builder and inside it, we can have another coroutine.
+    val gJob1 = GlobalScope.launch {
+        // `GlobalScope` is an independent coroutine without any relation to the parent coroutine.
+        // Inside the `GlobalScope`, we can have multiple coroutines.
+        // All the coroutines inside the `GlobalScope` are children of the `GlobalScope`.
+        // job1 is a child of the `GlobalScope` gJob1.
+        val job1 = launch {
+            repeat(5) {
+                println("runBlocking --> 1st GlobalScope.launch --> 1st launch --> before delay $it")
+                delay(1000)
+                println("runBlocking --> 1st GlobalScope.launch --> 1st launch --> after delay $it")
+            }
+            val job1insideJob1 = launch {
+                repeat(5) {
+                    println("runBlocking --> 1st GlobalScope.launch --> 1st launch --> launch --> before delay $it")
+                    delay(1000)
+                    println("runBlocking --> 1st GlobalScope.launch --> 1st launch --> launch --> after delay $it")
+                }
+            }
+            job1insideJob1.join()
+        }
+        // job2 is a child of the `GlobalScope` gJob1, and sibling of the job1.
+        val job2 = launch {
+            repeat(5) {
+                println("runBlocking --> 1st GlobalScope.launch --> 2nd launch --> before delay $it")
+                delay(1000)
+                println("runBlocking --> 1st GlobalScope.launch --> 2nd launch --> after delay $it")
+            }
+            launch {
+                repeat(5) {
+                    println("runBlocking --> 1st GlobalScope.launch --> 2nd launch --> launch --> before delay $it")
+                    delay(1000)
+                    println("runBlocking --> 1st GlobalScope.launch --> 2nd launch --> launch --> after delay $it")
+                }
+            }
+        }
+        job1.join()
+        job2.join()
+    }
+
+    val gJob2 = GlobalScope.launch {
+        val job1 = launch {
+            repeat(5) {
+                println("runBlocking --> 2nd GlobalScope.launch --> 1st launch --> before delay $it")
+                delay(1000)
+                println("runBlocking --> 2nd GlobalScope.launch --> 1st launch --> after delay $it")
+            }
+            launch {
+                repeat(5) {
+                    println("runBlocking --> 2nd GlobalScope.launch --> 1st launch --> launch --> before delay $it")
+                    delay(1000)
+                    println("runBlocking --> 2nd GlobalScope.launch --> 1st launch --> launch --> after delay $it")
+                }
+            }
+        }
+
+        val job2 = launch {
+            repeat(5) {
+                println("runBlocking --> 2nd GlobalScope.launch --> 2nd launch --> before delay $it")
+                delay(1000)
+                println("runBlocking --> 2nd GlobalScope.launch --> 2nd launch --> after delay $it")
+            }
+            launch {
+                repeat(5) {
+                    println("runBlocking --> 2nd GlobalScope.launch --> 2nd launch --> launch --> before delay $it")
+                    delay(1)
+                    println("runBlocking --> 2nd GlobalScope.launch --> 2nd launch --> launch --> after delay $it")
+                }
+            }
+        }
+        job1.join()
+        job2.join()
+    }
+    gJob1.join()
+    gJob2.join()
+}
