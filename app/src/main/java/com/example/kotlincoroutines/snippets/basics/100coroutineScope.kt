2c960b4fcf03e2bae7e0a126b03f272a39a4e70e
sagarpatel288 <sagar.patel@sharecare.com>
2025-02-23 17:41:34 +0530

    Document the use of `CoroutineScope` and `Job` cancellation.
    This commit introduces an example demonstrating the lifecycle of a `CoroutineScope` and how to cancel `Job` instances within it.
    It showcases how:
    -   A `Job` can be canceled independently of the `CoroutineScope`.
    -   The `CoroutineScope` remains active even after its `Job`s are canceled.
    -   The `CoroutineScope` must be canceled explicitly to stop the scope itself.
    - How to check if `Job` and `CoroutineScope` are active using `isActive`.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/basics/100coroutineScope.kt
@@ -0,0 +1,47 @@
+package com.example.kotlincoroutines.snippets.basics
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+class CoroutinePractice() {
+
+    /**
+     * In this example, we create a coroutine scope and a job inside it.
+     * We demonstrate how to cancel the job and check its status.
+     * We also demonstrate that the coroutine scope is still active after canceling the job,
+     * because it lives as long as the application is running.
+     *
+     * We have to manually cancel the coroutine scope.
+     */
+    suspend fun coroutinePractice() {
+        val coroutineScope = CoroutineScope(Dispatchers.IO + Job() + CoroutineName("myCoroutineScope"))
+        val job = coroutineScope.launch {
+            println("inside the coroutineScope")
+        }
+        println("Is the job active? ${job.isActive}")
+        println("Cancelling the job...")
+        delay(6000L)
+        job.cancel()
+        println("Is the job active? ${job.isActive}")
+        println("The coroutine scope is active: ${coroutineScope.coroutineContext.isActive}")
+        println("Cancelling the coroutine scope")
+        delay(3000L)
+        coroutineScope.coroutineContext.cancel()
+        println("The coroutine scope is active: ${coroutineScope.coroutineContext.isActive}")
+    }
+}
+
+fun main() {
+    runBlocking {
+        CoroutinePractice().coroutinePractice()
+    }
+}
+
