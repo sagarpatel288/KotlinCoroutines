67ce70a8e7a08c6236c38b4efcbfeece2e9abd5d
sagarpatel288 <sagarpatel288@gmail.com>
2025-02-23 19:07:20 +0530

    Implement Sequential and Concurrent Execution Examples with Coroutines.
    This commit introduces two new examples demonstrating different execution patterns with Kotlin coroutines:
    -   `concurrency`: Shows how launching multiple coroutines as siblings results in concurrent execution.
    -   `sequentialWaiting`: Demonstrates that instructions within a single coroutine block are executed sequentially, with subsequent operations waiting for the preceding ones to complete.

--- app/src/main/java/com/example/kotlincoroutines/snippets/basics/090sequentialConcurrency.kt
+++ app/src/main/java/com/example/kotlincoroutines/snippets/basics/090sequentialConcurrency.kt
@@ -8,6 +8,12 @@ import kotlin.system.measureTimeMillis
 
 class Concurrency() {
 
+    /**
+     * Siblings give concurrency. Siblings are co-operative tasks.
+     * So, if we launch multiple coroutines, it gives concurrency.
+     *
+     * job1 and job2 are siblings and they give concurrency.
+     */
     suspend fun concurrency() {
         coroutineScope {
             val job1 = launch {
@@ -27,7 +33,34 @@ class Concurrency() {
     }
 }
 
+class SequentialWaiting() {
+
+    /**
+     * However, a single coroutine block executes the code sequentially.
+     * Within the single and same coroutine block,
+     * the next instruction has to wait for the previous instruction to finish.
+     */
+    suspend fun sequentialWaiting() {
+        coroutineScope {
+            launch {
+                val time = measureTimeMillis {
+                    // All these instructions are executed sequentially as they are within the same coroutine block.
+                    println("sequentialWaiting --> GlobalScope.launch --> delay 1 --> ")
+                    delay(2000L)
+                    println("sequentialWaiting --> GlobalScope.launch --> delay 1 finished --> ")
+                    println("sequentialWaiting --> GlobalScope.launch --> delay 2 --> ")
+                    delay(2000L)
+                    println("sequentialWaiting --> GlobalScope.launch --> delay 2 finished --> ")
+                }
+                println("sequentialWaiting --> both the jobs took --> $time ms time")
+            }
+        }
+    }
+}
+
 fun main() = runBlocking {
     val concurrency = Concurrency()
     concurrency.concurrency()
+    val sequentialWaiting = SequentialWaiting()
+    sequentialWaiting.sequentialWaiting()
 }
