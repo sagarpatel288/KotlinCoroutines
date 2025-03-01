30b6455a6f646e9d5d5f98e5370b667accdc2861
sagarpatel288 <sagarpatel288@gmail.com>
2025-03-01 11:10:13 +0530

    Introduce explicit waiting for GlobalScope coroutines to finish.
    This commit introduces a new example that demonstrates how to explicitly wait for coroutines launched in the `GlobalScope` to complete before proceeding.
    It includes the following:
    - Launching two coroutines, `job1` and `job2`, within `GlobalScope`.
    - Launching another coroutine within `GlobalScope` that measures the execution time of `job1` and `job2` using `join()` and logs the result.
    - A `delay` added to `runBlocking` for allowing `GlobalScope` coroutines to execute.
    - The example demonstrates that reducing the `delay` may prevent the `GlobalScope` from executing, due to `runBlocking` ending sooner.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/basics/028explicitWaitForGlobalScope.kt
@@ -0,0 +1,37 @@
+package com.example.kotlincoroutines.snippets.basics
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlin.system.measureTimeMillis
+
+/**
+ * We put a `delay` before we finish the `runBlocking`.
+ * Hence, the `GlobalScope` may get a chance for the execution.
+ *
+ * If you reduce the `delay` to 1ms instead of 1000ms,
+ * maybe one of the `GlobalScope` will not get the chance for the execution.
+ *
+ * You can play with this example.
+ * Observe, how much time it takes for both the `GlobalScope` execution.
+ * And then, reduce (decrease) the `delay` than the execution time of both the `GlobalScope`.
+ * It may prevent one of the `GlobalScope` execution,
+ * because the `runBlocking` will end (finish) earlier before the `GlobalScope` gets a chance for the execution.
+ */
+fun main() = runBlocking {
+    val job1 = GlobalScope.launch {
+        println("Inside the 1st Global Scope - 01")
+    }
+    val job2 = GlobalScope.launch {
+        println("Inside the 2nd Global Scope - 02")
+    }
+    GlobalScope.launch {
+        val time = measureTimeMillis {
+            job1.join()
+            job2.join()
+        }
+        println("Both Global Scope execution finished after $time ms")
+    }
+    delay(1000L)
+}
