8b7b085f396f05703fdca15118383657c4503398
sagarpatel288 <sagarpatel288@gmail.com>
2025-03-01 11:00:07 +0530

    Introduce an example demonstrating explicit waiting for `GlobalScope` coroutines.
    This commit introduces a new example illustrating how to explicitly wait for coroutines launched within `GlobalScope` to complete.
    It includes the following changes:
    - The `explicitWaitForGlobalScope.kt` file includes a `main` function that launches two coroutines using `GlobalScope.launch`.
    - The first two launched coroutines, `job1` and `job2`, are intended to demonstrate independent operations within `GlobalScope`.
    - A third `GlobalScope.launch` block utilizes `job1.join()` and `job2.join()` to explicitly wait for the completion of `job1` and `job2`.
    - A `delay` in `main` is added to ensure the `GlobalScope` coroutines have a chance to execute before the `main` function finishes, showing that GlobalScope doesn't block the main function execution.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/basics/027explicitWaitForGlobalScope.kt
@@ -0,0 +1,25 @@
+package com.example.kotlincoroutines.snippets.basics
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+
+/**
+ * This may print the statements of each GlobalScope,
+ * because we do not finish the execution of the `main`.
+ * We have put a `delay` before we finish the `main` execution.
+ * It means, the `GlobalScope` may get a chance for the execution.
+ */
+suspend fun main() {
+    val job1 = GlobalScope.launch {
+        println("Inside the 1st Global Scope - 01")
+    }
+    val job2 = GlobalScope.launch {
+        println("Inside the 2nd Global Scope - 02")
+    }
+    GlobalScope.launch {
+        job1.join()
+        job2.join()
+    }
+    delay(2000L)
+}
