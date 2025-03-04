7cd57db1ea5b27490e942c259955d17fc53c6596
sagarpatel288 <sagarpatel288@gmail.com>
2025-03-04 12:58:36 +0530

    Introduce the Unconfined Dispatcher and its usage.
    This commit introduces a new example demonstrating the `Dispatchers.Unconfined` dispatcher in Kotlin coroutines.
    The `UnconfinedExample` class includes the `unconfinedExample` function, which shows how `Unconfined` starts execution in the current thread but can resume on different threads, particularly when other dispatchers like `Dispatchers.IO` are involved.
    The example shows that Unconfined dispatcher does not dispatch to other thread.
    The `main` function demonstrates the behavior by outputting log messages, each illustrating the current thread's name at different stages of the coroutine's execution, highlighting the dispatching behavior of the `Unconfined` and `IO` dispatchers.
    The code also includes a short description about when `Unconfined` dispatcher should be used.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/coroutineContexts/050unconfinedDispatcher.kt
@@ -0,0 +1,40 @@
+package com.example.kotlincoroutines.snippets.coroutineContexts
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * The `Unconfined` dispatcher starts (continues) the execution from the current call-frame (from which it was called),
+ * but resumes (finishes) on any thread that we might have launched in-between the unconfined block and is available.
+ *
+ * We normally don't use the unconfined dispatcher.
+ * However, while writing the test, we use `unconfinedTestDispatcher`.
+ *
+ * We will learn about the `unconfinedTestDispatcher` later.
+ *
+ * [Kotlin Doc](https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines/-dispatchers/-unconfined.html)
+ */
+class UnconfinedExample {
+
+    fun unconfinedExample() = runBlocking {
+        // Prints main
+        println("Thread: Inside runBlocking: ${Thread.currentThread().name}")
+        withContext(Dispatchers.Unconfined) {
+            // Prints main because we launched the unconfined dispatcher in the main thread.
+            println("Thread: runBlocking -> unconfined: ${Thread.currentThread().name}")
+            withContext(Dispatchers.IO) {
+                // Prints default-worker.
+                println("Thread: runBlocking -> unconfined -> IO -> ${Thread.currentThread().name}")
+            }
+            // Prints default-worker because we launched the `IO` dispatcher in-between.
+            println("Thread: runBlocking -> Unconfined -> after the IO, within Unconfined: ${Thread.currentThread()
+                .name}")
+        }
+    }
+}
+
+fun main() {
+    val example = UnconfinedExample()
+    example.unconfinedExample()
+}
