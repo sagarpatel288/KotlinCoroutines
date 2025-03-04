3c39de4643efef08aaeec0f725ea86c73af05d56
sagarpatel288 <sagarpatel288@gmail.com>
2025-03-04 12:44:46 +0530

    Introduce custom coroutine dispatcher.
    This commit introduces a `CustomDispatcher` class, which inherits from `CoroutineDispatcher`, allowing for the creation of a custom coroutine dispatcher.
    It overrides the mandatory `dispatch` method, although its implementation is currently left as a placeholder with a `TODO` comment.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/coroutineContexts/040customDispatcher.kt
@@ -0,0 +1,18 @@
+package com.example.kotlincoroutines.snippets.coroutineContexts
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Runnable
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * We can create our own custom dispatcher by inheriting `CoroutineDispatcher()`.
+ * We need to override the `dispatch()` method. It is mandatory.
+ * Other methods are optional.
+ */
+class CustomDispatcher: CoroutineDispatcher() {
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        TODO("Not yet implemented")
+    }
+}
+
