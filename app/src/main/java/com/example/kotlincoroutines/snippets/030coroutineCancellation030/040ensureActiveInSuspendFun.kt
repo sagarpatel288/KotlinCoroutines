a227fa1a4d108555d85cdb3fe5fe6e8f80da6979
sagarpatel288 <sagarpatel288@gmail.com>
2025-03-07 18:43:42 +0530

    Introduce ensureActive() for explicit coroutine cancellation checks in suspend functions.
    This commit introduces the `EnsureActiveInSuspendFun` class and the `suspendFunction` to demonstrate the use of `ensureActive()` for explicit cancellation checks within suspend functions.
    The `nestedScopesAndCoroutines` function now showcases:
    - The use of nested coroutines.
    - Cancelling an inner coroutine from the outer coroutine.
    - Execution flow across different dispatchers (`Dispatchers.IO` and `Dispatchers.Default`).
    - The importance of cooperative cancellation checks in long-running tasks.
    - The `suspendFunction` now includes:
      - `ensureActive()` to explicitly check for coroutine cancellation.
      - An explanation of how coroutines can continue executing if they do not check for cancellation.
      - An explanation of how coroutines check by default that whether the coroutine is active or not only when it starts a new coroutine or hits a suspension point.
      - An explanation of the scenarios where using `ensureActive()` is beneficial, especially in background tasks, CPU-intensive work, or UI updates.
    - This commit also includes a `main` function to execute `nestedScopesAndCoroutines`.

---
+++ app/src/main/java/com/example/kotlincoroutines/snippets/030coroutineCancellation030/040ensureActiveInSuspendFun.kt
@@ -0,0 +1,109 @@
+package com.example.kotlincoroutines.snippets.`030coroutineCancellation030`
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.ensureActive
+
+
+
+class EnsureActiveInSuspendFun {
+
+    fun nestedScopesAndCoroutines() = runBlocking {
+        val outerJob = CoroutineScope(Dispatchers.Default).launch {
+            val innerJob = launch {
+                suspendFunction()
+            }
+            delay(2000L)
+            innerJob.cancel()
+            // If the `suspendFunction` which is a part of the `innerJob` does not actively check for the cancellation,
+            // we may get some print statement from it even after cancelling the `innerJob`!
+            println("nestedScopesAndCoroutines: Cancelled the innerJob - suspendFunction")
+        }
+        outerJob.join()
+    }
+
+    /**
+     * This is an interesting example.
+     * The below suspend function does not handle or check for the coroutine cancellation.
+     *
+     * Now, at any point, one of the coroutine block, let us assume,
+     * the `withContext(Dispatchers.Default)` block has started its execution,
+     * and before it completes, we cancel the coroutine in which this suspend function has been called,
+     * it will not stop the ongoing execution of the `withContext(Dispatchers.Default)`,
+     * because there is no checkpoint inside the `withContext(Dispatchers.Default)` block,
+     * that checks whether the coroutine is cancelled or not.
+     *
+     * In this case, we are wasting our resources.
+     *
+     * However, if we cancel the coroutine inside which this suspend function has been called,
+     * before any of the coroutine inside the suspend block starts execution,
+     * let us assume, before the `withContext(Dispatchers.Default)`,
+     * then in this case, the `withContext(Dispatchers.Default)` will not start its execution.
+     *
+     * It means that, by default, a coroutine can check it by default that whether the coroutine is active or not,
+     * only when it starts a new coroutine.
+     *
+     * In other words, a coroutine can check it by default that whether the coroutine is active or not,
+     * only when it hits a suspension point.
+     *
+     * However, we can explicitly put check-points inside any coroutine using `ensureActive()` (recommended),
+     * `coroutineContext.isActive`, or by any inbuilt default suspend functions such as `yield()`, `delay()`, etc.
+     *
+     * Yes, kotlin in-built (pre-defined) suspend functions such as `yield()`, `delay()`, etc., explicitly check
+     * whether a coroutine is active or not.
+     *
+     * However, the straight-forward out-of-the-box, pre-defined, and recommended way to put this check-point is to use
+     * the `ensureActive()` function, which is an in-built, pre-defined function.
+     *
+     * The lesson here is:
+     * Whenever we use a suspend function, it is a good practice to use `ensureActive()`,
+     * especially during a background-work, CPU intensive work, or UI-update,
+     * because if the parent has already been cancelled, we don't want to waste our resources doing unnecessary
+     * computation and execution.
+     *
+     * Whether to use `ensureActive()` inside Android's `viewModelScope` depends on the situation.
+     *
+     * In most cases, it is safe to use `ensureActive()` inside the `viewModelScope`.
+     *
+     * For example, we don't stop the ongoing API call, file read/write, or database read/write, etc. when the user
+     * changes the configuration. E.g., rotating the screen, changing the theme or language, etc.
+     * But in this case, the `viewModelScope` survives anyway. So, we still have the active `viewModelScope`.
+     *
+     * However, when a user changes the screen, or to be precise, when we get `onCleared()` call of the
+     * `viewModelScope`, we want to make sure that any ongoing coroutine work inside the `viewModelScope` stops.
+     *
+     * Hence, it is safe to use `ensureActive()` inside a `viewModelScope` coroutines / suspend functions.
+     *
+     * In this example, we actively check for the cancellation.
+     * Hence, as soon as the parent coroutine (from which this `suspendFunction` has been called) is cancelled,
+     * any ongoing coroutine execution will also be cancelled (stopped).
+     *
+     */
+    private suspend fun suspendFunction() {
+        withContext(Dispatchers.IO) {
+            println("suspendFunction: Inside IO: Doing some blocking background work! Sleeping thread!")
+            Thread.sleep(2000L)
+            println("suspendFunction: Inside IO: Finished sleeping!")
+        }
+        withContext(Dispatchers.Default) {
+            repeat(100) {
+                ensureActive()
+                println("suspendFunction: Inside Default: repeating: $it times")
+                (1..10).map {
+                    it * it
+                }
+            }
+            println("suspendFunction: Inside the Default: Finished the CPU Intensive work!")
+        }
+        println("suspendFunction: At the end! Outside of both the withContext!")
+    }
+}
+
+fun main() {
+    EnsureActiveInSuspendFun().nestedScopesAndCoroutines()
+}
+
